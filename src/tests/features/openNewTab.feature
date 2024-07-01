4bc018169c47e8fdc9fea0cf764ec9a6bae777e8
paty <patyshagulg@gmail.com>
2024-07-01 14:34:47 -0500

    switch among pages methos is created and implemented

--- src/tests/features/openNewTab.feature
+++ src/tests/features/openNewTab.feature
@@ -3,7 +3,13 @@ Feature: Testing switching to tabs functionality
   Background:
     Given User navigates to OpenMrs page
 
-@regression
+  @only
   Scenario: Verify open a new tab functionality by clicking on TheOpenMRSEMR btn
     Given User clicks on Explore Our Emr button
     Then Verify user switched to a new tab "The OpenMRS EMR" title
+    And User switches back to first page
+    Then User sees three available buttons on OpenMRS main page
+      | options              |
+      | Explore Our EMR      |
+      | Join Our Community   |
+      | Learn in Our Academy |
--- src/tests/steps/openNewTabSteps.ts
+++ src/tests/steps/openNewTabSteps.ts
@@ -2,9 +2,11 @@ import { Given, When, Then } from "@cucumber/cucumber";
 import setUpClass from "../../../utils/setUpClass";
 import {expect} from "@playwright/test";
 import openNewTabPage from "../pages/openNewTabPage";
+import dataTablePage from "../pages/dataTablePage";
 
 
 let openNewTabpg=new openNewTabPage();
+let dataTablePg=new dataTablePage();
 
 
 Given('User clicks on Explore Our Emr button', async function () {
@@ -22,3 +24,44 @@ Given('User clicks on Explore Our Emr button', async function () {
 
    expect(actualText).toContain(expectedString);
   });
+
+
+
+Then('User switches back to first page', async function ()  {
+  await openNewTabpg.switchToParentPage();
+  await openNewTabpg.sleep(5000);
+})
+
+Then('User sees three available buttons on OpenMRS main page', async function (dataTable) {
+  
+  const data=dataTable.hashes();
+
+  const optionsFromFeature: string[] = [];
+  for (const row of data){
+      
+      let opt=row.options;
+      console.log("option from feature file from the loop: "+opt);
+
+      await optionsFromFeature.push(opt);     
+
+      
+  }
+
+  console.log("options from array feature file: "+optionsFromFeature);
+
+  const optionsFromUI: string[] = [];
+  const exploreOurEMRbtn:string=await openNewTabpg.getTextExploreOurEMRbtn();
+  console.log("actual for exploreOurEMR button from ui: "+exploreOurEMRbtn);
+   await optionsFromUI.push(exploreOurEMRbtn);
+
+   const joinOurCommunityBtn:string =await openNewTabpg.getTextjoinOurCommunityBtn();
+   console.log("actual for joinOurCommunityBtn button ui: "+joinOurCommunityBtn);   
+   await optionsFromUI.push((joinOurCommunityBtn));
+
+   const learnInOurAcademybtn:string=await openNewTabpg.getTextlearnInOurAcademybtn();
+   console.log("actual for learnInOurAcademybtn from UI: "+learnInOurAcademybtn);
+   await optionsFromUI.push(learnInOurAcademybtn);
+  
+
+   dataTablePg.compareArrays(optionsFromFeature,optionsFromUI);
+})
